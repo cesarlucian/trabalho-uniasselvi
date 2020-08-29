43dde5ea1458df9f2d234419002e0824bf06dd64
Leo Kiszewski <kiszewski1999@gmail.com>
2020-08-29 14:56:34 -0300

    criação da interface do formulario de cadastro

---
+++ views/templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Alunos</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+
+<body class="page">
+    <header class="header">
+        <div class="title">
+            Alunos
+        </div>
+    </header>
