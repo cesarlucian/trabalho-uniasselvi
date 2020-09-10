6649ad36152f46856bf03be975c43200f8e48e2f
Léo Kiszewski <kiszewski1999@gmail.com>
2020-09-09 22:57:15 -0300

    adicionado menu lateral

--- index.php
+++ index.php
@@ -5,7 +5,19 @@ include_once("projeto.templates/header.php");
 //carregarAlunos(Alunos::listaAlunoPag(''));
 ?>
 
-<p>index em desenvolvimento</p>
-<div class="button">
-            <a type="button" href="projeto.view/alunos/cadastro.php" class="btn btn-primary">Criar Aluno</a>
-        </div>
+<div class="menu-inicial">
+    <aside class="menu-lateral">
+        <ul>
+            <li><a href="#">Menu 1</a></li>
+            <li><a href="#">Menu 2</a></li>
+            <li><a href="#">Menu 3</a></li>
+            <li><a href="#">Menu 4</a></li>
+            <li><a href="#">Menu 5</a></li>
+        </ul>
+    </aside>
+
+    <p>index em desenvolvimento</p>
+    <div class="button">
+        <a type="button" href="projeto.view/alunos/cadastro.php" class="btn btn-primary">Criar Aluno</a>
+    </div>
+</div>
