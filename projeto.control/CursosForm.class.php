a88ad170710d6ec190db90ed8cf6bed3995aa2c6
Cesar Luciano <cesar.luciano2011@gmail.com>
2020-10-02 12:01:19 -0300

    ok

--- projeto.control/CursosForm.class.php
+++ projeto.control/CursosForm.class.php
@@ -48,12 +48,32 @@ class CursosForm {
                         <input class="form-control" type="text" name="ds_curso" id="ds_curso">
                     </div>
 
-                    <div class="col-md-2 col-lg-2">
-                        <label>Turma*</label>
+                    <div class="col-md-3 col-lg-3">
+                        <label>Tipo de turma</label>
+                        <select class="form-control" id="tipo_turma" name="tipo_turma" onchange="buscaFiltro();" required="true">
+                            <option>Selecione</option>
+                            <option value="1">Nova turma</option>
+                            <option value="2">Turmas dispon&iacute;veis</option>
+                        </select>
+                    </div>
+
+                    <div id="turmas_disponiveis" class="col-md-3 col-lg-3 hidden">
+                        <label>Turmas dispon&iacute;veis</label>
+                        <select class="form-control" id="turmas_disponiveis" name="turmas_disponiveis" required="true">
+                            <option>Nenhuma turma dispon&iacute;vel</option>
+                            <option>Turma 423</option>
+                            <option>Turma 333</option>
+                            <option>Turma 666</option>
+                            <option>Turma 546</option>
+                        </select>
+                    </div>
+
+                    <div id="nova_turma" class="col-md-2 col-lg-2 hidden">
+                        <label>Nova turma*</label>
                         <input class="form-control" type="number" id="turma'+i+'" name="turma[]" required="true">
                     </div>
 
-                    <div class="col-md-3 col-lg-3">
+                    <div id="botao_nova_turma" class="col-md-3 col-lg-3 hidden">
                         <label>&nbsp;</label><br>
                         <button type="button" class="btn btn-primary" href="#" id="addScnt">Adicionar turma</button>
                     </div>
@@ -76,34 +96,50 @@ class CursosForm {
             </form>
         </main>
         <script>
+            function buscaFiltro() {
 
+                var tipo_turma = document.getElementById("tipo_turma").value;
+
+                if(tipo_turma == 1 ){
+                    $("#nova_turma").removeClass('hidden');
+                    $("#botao_nova_turma").removeClass('hidden');
+                    $("#turmas_disponiveis").addClass('hidden');
+
+                } else if(tipo_turma == 2) {
+
+                    $("#nova_turma").addClass('hidden');
+                    $("#botao_nova_turma").addClass('hidden');
+                    $("#turmas_disponiveis").removeClass('hidden');
+                }
+            }
+                     
             $(function() {
-                    var scntDiv = $('#adiciona_turma');
-                    var i = $('#adiciona_turma p').size() + 1;
-                    
-                    $('#addScnt').live('click', function() {
-                            $('<p><label>Nova turma*</label><br><input class="campo1" type="number" id="turma'+i+'" name="turma[]" style="width:100px;" required="true"><a class="btn btn-primary" href="#" id="remScnt">Remover</a></p>').appendTo(scntDiv);
-                            i++;
+                var scntDiv = $('#adiciona_turma');
+                var i = $('#adiciona_turma p').size() + 1;
+                
+                $('#addScnt').live('click', function() {
+                        $('<p><label>Nova turma*</label><br><input class="campo1" type="number" id="turma'+i+'" name="turma[]" style="width:100px;" required="true"><a class="btn btn-primary" href="#" id="remScnt">Remover</a></p>').appendTo(scntDiv);
+                        i++;
 
-                            if( i == 6 ) {
-                                document.getElementById("addScnt").disabled = true;
-                            }
+                        if( i == 6 ) {
+                            document.getElementById("addScnt").disabled = true;
+                        }
 
-                            return false;
-                    });
-                    
-                    $('#remScnt').live('click', function() { 
-                            if( i > 1 ) {
-                                $(this).parents('p').remove();
-                                i--;
-                            }
+                        return false;
+                });
+                
+                $('#remScnt').live('click', function() { 
+                        if( i > 1 ) {
+                            $(this).parents('p').remove();
+                            i--;
+                        }
 
-                            if(i < 6) {
+                        if(i < 6) {
 
-                                document.getElementById("addScnt").disabled = false;
-                            }
-                            return false;
-                    });
+                            document.getElementById("addScnt").disabled = false;
+                        }
+                        return false;
+                });
             });
         </script>
 
