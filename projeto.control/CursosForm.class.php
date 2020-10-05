96f2d0b1047d16a44762b77bfff22dcdc900c5c4
Cesar Luciano <cesar.luciano2011@gmail.com>
2020-10-05 14:05:37 -0300

    Ok

--- projeto.control/CursosForm.class.php
+++ projeto.control/CursosForm.class.php
@@ -48,42 +48,6 @@ class CursosForm {
                         <input class="form-control" type="text" name="ds_curso" id="ds_curso" required="true">
                     </div>
 
-                    <div class="col-md-4 col-lg-4">
-                        <label>Tipo de turma</label>
-                        <select class="form-control" id="tipo_turma" name="tipo_turma" onchange="buscaFiltro();" required="true">
-                            <option value="">Selecione</option>
-                            <option value="1">Nova turma</option>
-                            <option value="2">Turmas dispon&iacute;veis</option>
-                        </select>
-                    </div>
-
-                    <div id="turmas_disponiveis" class="col-md-3 col-lg-3 hidden">
-                        <label>Turmas dispon&iacute;veis</label>
-                        <select class="form-control" id="turma_disponivel" name="turma_disponivel">
-                            <option value="">Nenhuma turma dispon&iacute;vel</option>
-                            <option value="423">Turma 423</option>
-                            <option value="333">Turma 333</option>
-                            <option value="666">Turma 666</option>
-                            <option value="546">Turma 546</option>
-                        </select>
-                    </div>
-
-                    <div id="nova_turma" class="col-md-3 col-lg-3 hidden">
-                        <label>Nova turma*</label>
-                        <div class="input-group">
-                            <input class="form-control" type="number" id="turma" name="turma[]">
-                            <div class="input-group-btn">
-                               <button type="button" class="btn btn-primary" href="#" id="addScnt">+ Adicionar</button>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <br><div class="row">
-                    <div class="col-md-5 col-lg-5" id="adiciona_turma">
-                        
-                    </div>
                 </div>
 
                 <div class="col-md-12 col-lg-12">
@@ -96,65 +60,6 @@ class CursosForm {
             </form>
         </main>
         <script>
-            function buscaFiltro() {
-
-                var tipo_turma = document.getElementById("tipo_turma").value;
-                var turma = document.getElementById("turma").value;
-                var turma_disponivel = document.getElementById("turma_disponivel").value;
-
-                if(tipo_turma == 1 ){
-                    $("#nova_turma").removeClass('hidden');
-                    $("#turmas_disponiveis").addClass('hidden');
-
-                    document.getElementById("turma").required = true;
-                    document.getElementById("turma_disponivel").required = false;
-
-                } else if(tipo_turma == 2) {
-
-                    $("#nova_turma").addClass('hidden');
-                    $("#turmas_disponiveis").removeClass('hidden');
-
-                    document.getElementById("turma").required = false;
-                    document.getElementById("turma_disponivel").required = true;
-
-                } else {
-
-                    $("#nova_turma").addClass('hidden');
-                    $("#turmas_disponiveis").addClass('hidden');
-
-                    document.getElementById("turma").required = true;
-                    document.getElementById("turma_disponivel").required = true;
-                }
-            }
-                     
-            $(function() {
-                var scntDiv = $('#adiciona_turma');
-                var i = $('#adiciona_turma label').size() + 1;
-                
-                $('#addScnt').live('click', function() {
-                    $('<label>Turma '+i+'*<input class="form-control" type="number" id="turma" name="turma[]" style="width:100px;" required="true"><a class="btn btn-danger mx-3" href="#" id="remScnt">Remover</a></label>').appendTo(scntDiv);
-                    i++;
-
-                    if( i == 5 ) {
-                        document.getElementById("addScnt").disabled = true;
-                    }
-
-                    return false;
-                });
-                
-                $('#remScnt').live('click', function() { 
-                    if( i > 1 ) {
-                        $(this).parents('label').remove();
-                        i--;
-                    }
-
-                    if(i < 5) {
-
-                        document.getElementById("addScnt").disabled = false;
-                    }
-                    return false;
-                });
-            });
         </script>
 
         <?php
