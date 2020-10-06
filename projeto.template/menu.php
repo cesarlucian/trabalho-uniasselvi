b00b1dea3c767c91f42d864f124f1ab8a857525b
Cesar Luciano <cesar.luciano2011@gmail.com>
2020-10-06 10:17:56 -0300

    ok

--- projeto.template/menu.php
+++ projeto.template/menu.php
@@ -3,9 +3,9 @@
                 <ul class="sidebar-menu">         
                     <li><a href="/trabalho-uniasselvi">Início</a></li>
                     <li><a href="/trabalho-uniasselvi/projeto.view/alunos/index.php">Alunos</a></li>
-                    <li><a href="/trabalho-uniasselvi/projeto.view/chamada/index.php">Chamada</a></li>
                     <li><a href="/trabalho-uniasselvi/projeto.view/cursos/index.php">Cursos</a></li>
                     <li><a href="/trabalho-uniasselvi/projeto.view/turmas/index.php">Turmas</a></li>
+                    <li><a href="/trabalho-uniasselvi/projeto.view/chamada/index.php">Chamada</a></li>
                 </ul>
             </aside>
         
