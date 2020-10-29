cfb394568f4bcd3d53f0068f8b699dd690e82787
Cesar Luciano <cesar.luciano2011@gmail.com>
2020-10-29 10:43:01 -0300

    atualizado formato de validar tipos de arquivos permitidos

--- projeto.control/FaltasJustificadasForm.class.php
+++ projeto.control/FaltasJustificadasForm.class.php
@@ -30,7 +30,7 @@ class FaltasJustificadasForm {
                     <div class="row">
                         <div class="col-lg-6 col-md-6">
                             <br><label>Anexo*</label>
-                            <input type="file" class="form-control" name="nm_arquivo" id="nm_arquivo" required="true">
+                            <input type="file" class="form-control" name="nm_arquivo" id="nm_arquivo" required="true" max-size="20000" accept=".jpeg,.jpg,.png,.pdf,.doc,.docx">
                         </div>
 
                         <div class="col-lg-6 col-md-6">
@@ -43,7 +43,7 @@ class FaltasJustificadasForm {
                     <div class="row">
                         <div class="col-lg-12 col-md-12">
                             <br><label>Motivo*</label>
-                            <textarea class="form-control" name="ds_motivo" id="ds_motivo" maxlength="600" required="true"></textarea>
+                            <textarea placeholder="Insira o motivo..."class="form-control" name="ds_motivo" id="ds_motivo" maxlength="600" required="true"></textarea>
                         </div>
                     </div>
                     
@@ -55,8 +55,15 @@ class FaltasJustificadasForm {
                     </div> 
                 </div>
             </form>
-            <script type="text/javascript">
-                
+            <script>
+               var uploadField = document.getElementById("nm_arquivo");
+
+                uploadField.onchange = function() {
+                    if(this.files[0].size > 30000){
+                       alert("Arquivo muito grande! 30kbytes permitido.");
+                       this.value = "";
+                    };
+                };
             
                 function fechaJanela() {
                     opener.location.reload();
--- projeto.view/faltas/faltas_man.php
+++ projeto.view/faltas/faltas_man.php
@@ -11,56 +11,25 @@ if(isset($evento)){
 
         case 'nova_falta':
 
-        $aluno = new Alunos();
-        $aluno->getObject($cd_aluno);
+            $aluno = new Alunos();
+            $aluno->getObject($cd_aluno);
 
-        $faltas = new FaltasJustificadas();
-        $chamada = new Chamada();
+            $faltas = new FaltasJustificadas();
+            $chamada = new Chamada();
 
-        $extensoes_permitidas = array('jpeg', 'jpg', 'png','pdf', 'doc', 'docx');
+            $file = $_FILES["nm_arquivo"];
+            $pasta_destino = "../../projeto.arquivos";
+            $arquivo_ext = explode(".",$file["name"]);
+            $ext = $arquivo_ext[1];
 
-        $file = $_FILES["nm_arquivo"];
-        $pasta_destino = "../../projeto.arquivos";
-        $arquivo_ext = explode(".",$file["name"]);
-        $ext = $arquivo_ext[1];
+            // ------------------------
 
-        // ------------------------
+            /* metodo servidor upload 
+            
+            $upload = new Upload;
+            $upload->efetuaUpload('trabalho-uniasselvi/projeto.arquivos/'.$file, $file['tmp_name']);
 
-        /* metodo servidor upload 
-        
-        $upload = new Upload;
-        $upload->efetuaUpload('trabalho-uniasselvi/projeto.arquivos/'.$file, $file['tmp_name']);
-
-        */
-
-        if($file["size"] > "30000") {
-
-            ?>
-                <script>
-                    alert("Erro ao registrar falta, arquivo muito grande! M\u00e1ximo 30kb");
-                    history.back();
-                </script>
-            <?php
-
-            $file = fopen("../../projeto.log/log.txt","a+");
-            fwrite($file,"Erro ao registrar falta, arquivo muito grande, máximo 30kb - ".date("Y-m-d H:i:s")."\r\n");
-            fclose($file);
-
-        } else if(!in_array($ext, $extensoes_permitidas)) {
-
-            ?>
-                <script>
-                    alert("Erro ao registrar falta, tipo de arquivo inv\u00e1lido!");
-                    history.back();
-                </script>
-            <?php
-
-            $file = fopen("../../projeto.log/log.txt","a+");
-            fwrite($file,"Erro ao registrar falta, arquivo com extensão não permitida - ".date("Y-m-d H:i:s")."\r\n");
-            fclose($file);
-
-
-        } else {
+            */
 
             $chamada->removeFalta($dt_falta,$cd_aluno);
 
@@ -81,8 +50,6 @@ if(isset($evento)){
             fwrite($file,"Falta justificada registrada com sucesso - ".date("Y-m-d H:i:s")."\r\n");
             fclose($file);
             
-        }
-
         break;
     }
 }
